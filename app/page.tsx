c31349818265f81b86c90c79e0ec33cfaa0ff436
Aditya Kulkarni <100509496+kaditya123@users.noreply.github.com>
2024-08-21 02:13:41 +0530

    Delete app/page.tsx
    app/page.tsx deleted (not required)

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    // <p className="text-7xl text-blue-500">Hello AI Saas</p>
-     <Button variant = "destructive" size="lg" >Click ME</Button>
-  );
-}
